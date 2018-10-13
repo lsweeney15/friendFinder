493865ca8703aaad05c2674eb5f1f6aeffe04301
Liam Sweeney <liamsweeney@liam-sweeneys-computer.local>
2018-10-13 14:20:16 -0400

    not working

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,43 +1,37 @@
-var friends = require("../data/friends");
-var path = require('path');
 
-module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
-        res.json(friends);
-    });
+var friends = require("../data/friends.js");
 
-    app.post("/app/friends", function (req, res) {
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 100
-        };
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+  app.post("/api/friends", function(req, res) {
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+    var userData = req.body;
+    var userScores = userData.scores;
+    var totalDifference = 0;
 
-
-        var userData = req.body;
-        var userScores = userData.scores;
-
-        console.log(userScores);
-
-        var totalDifference = 0;
-        for (var i = 0; i < friends.length; i++) {
-
-            console.log(friends[i].name);
-            totalDifference = 0;
-
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-
-                if (totalDifference <= bestMatch.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
-
-                }
-            }
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+      totalDifference = 0;
+      for (var j = 0; j < friends[i].scores[j]; j++) {
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+        if (totalDifference <= bestMatch.friendDifference) {
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
         }
-        friends.push(userData);
+        console.log(totalDifference);
+      }
+    }
 
-        res.json(bestMatch);
-    });
+    friends.push(userData);
+    res.json(bestMatch);
+
+  });
+
 };
--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json()
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-   });
+    console.log("Your online using: " + PORT);
+});
